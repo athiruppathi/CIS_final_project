da075031ed0c7224ca8b7605da307de651feb650
athiruppathi <arjunthiruppathi@gmail.com>
2020-11-10 15:39:48 -0500

    fixed bugs in data import

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from jobs.spiders import monster
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import pandas as pd 
+import os
 
 # Create Tkinter root window
 root = tk.Tk()
@@ -32,17 +33,21 @@ process.crawl(monster.MonsterSpider)
 process.start()
 
 # Import and Clean Data 
-#pd.read_csv()
-indeed_sdata = pd.read_csv(r"C:\Users\arjun\OneDrive\Desktop\jobs\indeed_data.csv")
-linkedin_sdata = pd.read_csv(r"C:\Users\arjun\OneDrive\Desktop\jobs\linkedin_data.csv")
-monster_sdata = pd.read_csv(r"C:\Users\arjun\OneDrive\Desktop\jobs\monster_data.csv")
+current_directory = os.getcwd()
+partialPathIndeed = '\indeed_data.csv'
+partialPathLinkedin = '\linkedin_data.csv'
+partialPathMonster = '\monster_data.csv'
+
+indeedPath = current_directory + partialPathIndeed
+linkedinPath = current_directory + partialPathLinkedin
+monsterPath = current_directory + partialPathMonster
+
+indeed_sdata = pd.read_csv(indeedPath)
+linkedin_sdata = pd.read_csv(linkedinPath)
+monster_sdata = pd.read_csv(monsterPath)
+
 
 #Indeed 
-indeed_sdata.drop(columns=['linkedin_links','linkedin_titles','monster_links','monster_titles'], axis=1, inplace=True) #remove extra columns
-indeed_sdata.drop([1], inplace=True) #remove extra row
-indeed_sdata.reset_index(inplace=True)  #reset the index
-indeed_sdata.drop(['index'], axis=1, inplace=True) #remove the old index column
-
 
 link_string1 = indeed_sdata.iloc[0,0]
 link_string1 = link_string1[1:-2]
@@ -68,42 +73,36 @@ indeed_df = pd.DataFrame({'titles':indeed_titles_list,'links':indeed_links_list}
 
 #Linkedin
 linkedin_sdata.drop(['indeed_links','indeed_titles','monster_links','monster_titles'], axis=1, inplace=True)
-linkedin_sdata.drop([1,3,5,6,7,9,10], inplace=True)
+linkedin_sdata.drop([1,2,3,4], axis=0, inplace=True)
 linkedin_sdata.reset_index(inplace=True)
 linkedin_sdata.drop(['index'], axis=1, inplace=True)
 
 linksA = linkedin_sdata.iloc[0,0]
-linksB = linkedin_sdata.iloc[1,0]
-linksC = linkedin_sdata.iloc[2,0]
-linksD = linkedin_sdata.iloc[3,0]
-
-linkedin_links_list = linksA + ',' + linksB + ',' + linksC + ',' + linksD
-linkedin_links_list = linkedin_links_list.split(",")
+linkedin_links_list = linksA.split(",")
 
 titlesA = linkedin_sdata.iloc[0,1]
-titlesB = linkedin_sdata.iloc[1,1]
-titlesC = linkedin_sdata.iloc[2,1]
-titlesD = linkedin_sdata.iloc[3,1]
-
-linkedin_titles_list = titlesA + ',' + titlesB + ',' + titlesC + ',' + titlesD
-linkedin_titles_list = linkedin_titles_list.split(",")
-
-del linkedin_titles_list[97:-1]
+linkedin_titles_list = titlesA.split(',')
 
 linkedin_df = pd.DataFrame({'titles':linkedin_titles_list, 'link':linkedin_links_list}) #linkedin dataframe
 
 
 #Monster
-monster_sdata.drop(['indeed_links','indeed_titles','linkedin_links','linkedin_titles'], axis=1, inplace=True)
-
-monster_links_string = monster_sdata.iloc[0,0]
+monster_links_string = monster_sdata.iloc[0,4]
 monster_links_list = monster_links_string.split(",")
 
-monster_titles_string = monster_sdata.iloc[0,1]
+monster_titles_string = monster_sdata.iloc[0,5]
 monster_titles_list = monster_titles_string.split(",")
 
+for i in monster_titles_list:  # account for possibility of city,state format splititng at the comma 
+    if len(i) < 7:
+        indexValue = monster_titles_list.index(i)
+        monster_titles_list.pop(indexValue)
+
+    
 monster_df = pd.DataFrame({'titles':monster_titles_list,'links':monster_links_list})  # monster dataframe
 
+
+# Make entries in the GUI that dispaly job entries from dataframes
 
 
 
